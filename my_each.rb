0917878eba6be3fc31fbe0ff391f5de9d9c6a5e2
Matthew Prout <mdprout88@gmail.com>
2017-01-25 21:29:50 -0500

    seperated my_each_with_index to a seperate file

--- my_each.rb
+++ my_each.rb
@@ -7,12 +7,5 @@ module Enumerable
             i += 1
        end
    end
-    
-    def my_each_with_index
-       i = 0
-        while i < self.length
-            yield(self[i],i)
-            i+=1
-    end
-    
+
 end
